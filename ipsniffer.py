0d3a2773d263ca6b7a515d867f23ca6adfedf9ae
Paolo Bellagente <p.bellagente@unibs.it>
2018-07-24 16:04:09 +0200

    code cleaning and doc enhance

--- ipsniffer.py
+++ ipsniffer.py
@@ -1,52 +1,39 @@
 import logging
 import multiprocessing, time
 from scapy.all import *
-# # necessary to configure scapy to sniff with pcap (and consider the VLAN header)
-# conf.use_pcap = True
-# import scapy.arch.pcapdnet
-import netifaces
 
-
-class ipsniffer:
+class IPsniffer:
     """
     Implements a network sniffer using the scapy library.
     """
 
-    # class PacketHandler:
-    #     """
-    #     defines the handler of the packets sniffed by the worker
-    #     """
-    #
-    #     def __init__(self, filter, iface, buffer):
-    #         self.q = buffer
-    #         self.filter = filter
-    #         self.iface = iface
-    #
-    #     def get_pkt(self, pkt):
-    #         self.q.put(pkt)
-    #         # print "packet received "+str(self.q.qsize())
-
     def __init__(self, name, iface, filter):
+        """
+        Inits the sniffer
+        :param name: is the name assigned to the process
+        :param iface:  is the interface name string
+        :param filter: is the packet filter in tcpdump syntax
+        """
         self.name = name
         self.buffer = multiprocessing.Queue()
         self.iface = iface
         self.filter = filter
-        # self.handler = self.PacketHandler(filter, iface, self.buffer)
         self.p = self._set_process()
         self.p.daemon = True
 
     def _set_process(self):
-        return multiprocessing.Process(name=self.name, target=self._sniffer, args=(self.filter, self.iface, self.buffer,))
+        """
+        creates the background process
+        :return:
+        """
+        return multiprocessing.Process(name=self.name, target=self._sniffer,
+                                       args=(self.filter, self.iface, self.buffer,))
 
     def _sniffer(self, filter, iface, mybuffer):
         """
         Implements e sniffer to be started in background
         :return:
         """
-        print "worker started"
-        # while True:
-        #     buffer.put("a")
-        #     time.sleep(1)
         sniff(iface=iface, filter=filter, prn=lambda pkt: mybuffer.put(pkt), store=0)
 
     def start(self):
@@ -60,14 +47,9 @@ class ipsniffer:
         self.p.start()
         print "started"
 
-
     def kill(self):
         """
         Kills the sniffer thread.
         :return:
         """
         self.p.terminate()
-
-
-if __name__ == '__main__':
-    pass
